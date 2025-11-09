4eeea693b6a9f5ffd8ce428fc12225aab5003ed4
MKF343 <kirtland100@gmail.com>
2025-11-09 17:30:13 -0500

    Update userquery.sql

--- database/userquery.sql
+++ database/userquery.sql
@@ -5,5 +5,5 @@ CREATE USER 'recipe_app_user'@'localhost' IDENTIFIED BY 'Kz8gnHfufP4b';
 GRANT SELECT, INSERT, UPDATE, DELETE ON recipebox.recipes
 TO 'recipe_app_user'@'localhost';
 
--- Step 3: Apply the changes
+
 FLUSH PRIVILEGES;
